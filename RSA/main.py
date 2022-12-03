c86bdd106321737e1d7ac0db011bfc52d4797ae1
matwej29 <matweej2004@yandex.ru>
2022-12-03 18:54:21 +0300

    Update main.py

--- RSA/main.py
+++ RSA/main.py
@@ -1,11 +1,35 @@
 from math import isqrt
+import numpy as np
 
 
-def sieve_of_eratosthenes(n):
+def sieve_of_erathofenes(n):
+    numbers = np.arange(n + 1)
+    numbers[0], numbers[1] = 0, 0
+    numbers_len = numbers.size
+    i = 2
+
+    while i < n:
+        while i < numbers_len - 1 and not numbers[i]:
+            i += 1
+        p = numbers[i]
+
+        for j in range(0, numbers_len):
+            shuttle = p ** 2 + p * j
+            if shuttle >= numbers_len:
+                break
+            numbers[shuttle] = 0
+        i += 1
+    return numbers
+
+
+# print(sieve_of_erathofenes(1000)[-100:])
+
+
+def find_prime_factorisation(n):
     sq_n = isqrt(n) + 1
-    numbers = [i for i in range(n + 1)]
-    numbers[0], numbers[1] = False, False
-    numbers_len = n
+    numbers = np.arange(sq_n + 1)
+    numbers[0], numbers[1] = 0, 0
+    numbers_len = numbers.size
     i = 2
 
     while i < sq_n:
@@ -13,25 +37,17 @@ def sieve_of_eratosthenes(n):
             i += 1
         p = numbers[i]
 
+        q = n // p
+        if n % p == 0:
+            return p, q
+
         for j in range(0, numbers_len):
             shuttle = p ** 2 + p * j
-            if shuttle > numbers_len:
+            if shuttle >= numbers_len:
                 break
-            numbers[shuttle] = False
+            numbers[shuttle] = 0
         i += 1
     return numbers
-
-
-def find_prime_factorisation(n):
-    sieve = sieve_of_eratosthenes(n)
-
-    for p in sieve:
-        if not p:
-            continue
-        q = sieve[n // p]
-        if n % p == 0 and q:
-            return p, q
-    return None
 
 
 print(find_prime_factorisation(1714347136984384163159))
