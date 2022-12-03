f022e1c6189133141d2ddf25b41b511d3405493b
matwej29 <51135127+matwej29@users.noreply.github.com>
2022-12-03 22:11:05 +0300

    many memory

--- RSA/main.py
+++ RSA/main.py
@@ -27,17 +27,17 @@ def sieve_of_erathofenes(n):
 
 def find_prime_factorisation(n):
     sq_n = isqrt(n) + 1
-    numbers = np.arange(sq_n + 1)
+    numbers = np.empty(sq_n + 1, bool)
     numbers[0], numbers[1] = 0, 0
     numbers_len = numbers.size
     i = 2
 
     while i < sq_n:
-        while i < numbers_len - 1 and not numbers[i]:
+        while i < numbers_len and not numbers[i]:
             i += 1
-        p = numbers[i]
-
+        p = i
         q = n // p
+        # print(p, q)
         if n % p == 0:
             return p, q
 
@@ -45,9 +45,10 @@ def find_prime_factorisation(n):
             shuttle = p ** 2 + p * j
             if shuttle >= numbers_len:
                 break
-            numbers[shuttle] = 0
+            numbers[shuttle] = False
         i += 1
     return numbers
 
 
-print(find_prime_factorisation(1714347136984384163159))
+# print(find_prime_factorisation(19*23))
+print(find_prime_factorisation(3575243910821364495641))
