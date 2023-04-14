1a51e2d6744dce50599cc9c8267d0e11086c7cb3
matwej29 <matweej2004@yandex.ru>
2023-04-14 13:33:00 +0300

    add lesson9/4

---
+++ practicum_kurs1_sem2/lesson_9/t4_shunting_yard_algorithm.py
@@ -0,0 +1,71 @@
+OPERATIONS = ['+', '-', '*', '/', '**']
+
+PRIORITY = {
+    '+': 5,
+    '-': 5,
+    '*': 4,
+    '/': 4,
+    '**': 3
+}
+
+LEFT = 'left'
+RIGHT = 'right'
+
+ASSOCIATIVITY = {
+    '+': LEFT,
+    '-': LEFT,
+    '*': LEFT,
+    '/': LEFT,
+    '**': RIGHT
+}
+
+
+def shunting_yard_algorithm(inf):
+    stack = []
+    ops = []
+
+    for lexeme in inf:
+        if lexeme.isdigit():
+            ops.append(lexeme)
+        elif lexeme == "(":
+            stack.append(lexeme)
+        elif lexeme == ")":
+            founded = False
+            while len(stack) > 0:
+                elem = stack.pop()
+                if elem == "(":
+                    founded = True
+                    break
+                else:
+                    ops.append(elem)
+            if not founded:
+                raise Exception("Notation is not correct!")
+        elif lexeme in OPERATIONS:
+            if len(stack) == 0 or stack[-1] == "(":
+                stack.append(lexeme)
+            # менее приоритетная
+            elif PRIORITY[stack[-1]] > PRIORITY[lexeme]:
+                stack.append(lexeme)
+            else:
+                # более приоритетная
+                while PRIORITY[stack[-1]] < PRIORITY[lexeme] or (
+                        PRIORITY[stack[-1]] == PRIORITY[lexeme] and ASSOCIATIVITY[stack[-1]] == LEFT):
+                    ops.append(stack.pop())
+                    if len(stack) == 0: break
+                stack.append(lexeme)
+
+    while len(stack) > 0:
+        elem = stack.pop()
+        if elem == "(":
+            raise Exception("Notation is not correct!")
+
+        ops.append(elem)
+
+    return ' '.join(ops)
+
+
+if __name__ == '__main__':
+    try:
+        print(shunting_yard_algorithm(input().split()))
+    except Exception:
+        print("error")
