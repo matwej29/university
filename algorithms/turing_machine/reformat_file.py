bbda7b753e2605c49f82755ce9dc2f4350499557
matwej29 <matweej2004@yandex.ru>
2023-05-06 08:14:35 +0300

    update turing

--- algorithms/turing_machine/reformat_file.py
+++ algorithms/turing_machine/reformat_file.py
@@ -1,21 +1,26 @@
-with open("task.txt", 'r') as f:
-    lines = f.readlines()
+def reformat():
+    with open("task.txt", 'r') as f:
+        lines = f.readlines()
 
-result = []
+    result = []
 
-for i, line in enumerate(lines):
-    if "{" not in line:
-        continue
-    else:
-        lines = lines[i+1:]
-        break
+    for i, line in enumerate(lines):
+        if "{" not in line:
+            continue
+        else:
+            lines = lines[i + 1:]
+            break
 
-for i, line in enumerate(lines):
-    if ";" not in line: continue
-    result.append(line.split(';')[0].strip())
-    if "}" in line: break
+    for i, line in enumerate(lines):
+        if ";" not in line: continue
+        result.append(line.split(';')[0].strip())
+        if "}" in line: break
 
-result = '\n'.join(result)
+    result = '\n'.join(result)
 
-with open("res", 'w') as f:
-    f.writelines(result)
+    with open("res", 'w') as f:
+        f.writelines(result)
+
+
+if __name__ == '__main__':
+    reformat()
