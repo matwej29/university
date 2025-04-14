28e5629ed40826839e9199709398f158be830db3
matwej29 <matweej2004@yandex.ru>
2025-04-14 22:11:01 +0300

    image processing: some refactoring

--- image_processing/src/Zoom.cpp
+++ image_processing/src/Zoom.cpp
@@ -47,8 +47,48 @@ public:
     zoomImage();
   }
 
-  void calculate_pixel() {
+  double calculate_interpolated_color(
+      const double x, const double y, const int u, const int v,
+      std::vector<std::vector<double>> &img_color_frame) {
+
+    std::array<double, 4> y_interpolation;
+    for (int row = 0; row < 4; ++row) {
+      std::vector<std::vector<double>> mat(4, std::vector<double>(5));
+      for (int col = 0; col < 4; ++col) {
+        double xv = (double)col;
+        mat[col][0] = xv * xv * xv;
+        mat[col][1] = xv * xv;
+        mat[col][2] = xv;
+        mat[col][3] = 1.;
+        mat[col][4] = img_color_frame[row][col];
+      }
+
+      const auto coeffs = Gauss::solve_mat(mat);
+      const double tx = x - u;
+      const double interpolated = coeffs[0] * tx * tx * tx +
+                                  coeffs[1] * tx * tx + coeffs[2] * tx +
+                                  coeffs[3];
+
+      y_interpolation[row] = interpolated;
+    }
+
+    std::vector<std::vector<double>> mat(4, std::vector<double>(5));
+    for (int row = 0; row < 4; ++row) {
+      double xv = (double)row;
+      mat[row][0] = xv * xv * xv;
+      mat[row][1] = xv * xv;
+      mat[row][2] = xv;
+      mat[row][3] = 1.;
+      mat[row][4] = y_interpolation[row];
+    }
+    const auto coeffs = Gauss::solve_mat(mat);
+    const double ty = y - v;
+    const double interpolated = coeffs[0] * ty * ty * ty + coeffs[1] * ty * ty +
+                                coeffs[2] * ty + coeffs[3];
+    return interpolated;
   }
+
+  void calculate_pixel() {}
 
 #pragma optimize("t") clang loop vectorize(enable) interleave(enable)
   void zoomImage() {
@@ -77,54 +117,13 @@ public:
         std::vector<std::vector<double>> img_color_frame(
             4, std::vector<double>(4));
 
-        const auto calculate_interpolated_color = [&]() {
-          std::array<double, 4> y_interpolation;
-          // for each horizontal line
-          for (int row = 0; row < 4; ++row) {
-            std::vector<std::vector<double>> mat(4, std::vector<double>(5));
-            for (int col = 0; col < 4; ++col) {
-              double xv = (double)col;
-              mat[col][0] = xv * xv * xv;
-              mat[col][1] = xv * xv;
-              mat[col][2] = xv;
-              mat[col][3] = 1.;
-              mat[col][4] = img_color_frame[row][col];
-            }
-
-            // Solve for a,b,c,d
-            const auto coeffs = Gauss::solve_mat(mat);
-            const double tx = x - u;
-            const double interpolated = coeffs[0] * tx * tx * tx +
-                                        coeffs[1] * tx * tx + coeffs[2] * tx +
-                                        coeffs[3];
-
-            // Store this horizontal result in a temporary array
-            y_interpolation[row] = interpolated;
-          }
-          std::vector<std::vector<double>> mat(4, std::vector<double>(5));
-          for (int row = 0; row < 4; ++row) {
-            double xv = (double)row;
-            mat[row][0] = xv * xv * xv;
-            mat[row][1] = xv * xv;
-            mat[row][2] = xv;
-            mat[row][3] = 1.;
-            mat[row][4] = y_interpolation[row];
-          }
-          const auto coeffs = Gauss::solve_mat(mat);
-          const double ty = y - v;
-          const double interpolated = coeffs[0] * ty * ty * ty +
-                                      coeffs[1] * ty * ty + coeffs[2] * ty +
-                                      coeffs[3];
-          return interpolated;
-        };
-
         for (int i = -1; i <= 2; ++i) {
           for (int j = -1; j <= 2; ++j) {
             img_color_frame[j + 1][i + 1] =
                 GetImageColor(img.orgImg, u + i, v + j).r;
           }
         }
-        c.r = std::round(calculate_interpolated_color());
+        c.r = std::round(calculate_interpolated_color(x, y, u, v, img_color_frame));
         c.r = std::clamp<unsigned char>(c.r, 0, 255);
 
         for (int i = -1; i <= 2; ++i) {
@@ -133,7 +132,7 @@ public:
                 GetImageColor(img.orgImg, u + i, v + j).g;
           }
         }
-        c.g = std::round(calculate_interpolated_color());
+        c.g = std::round(calculate_interpolated_color(x, y, u, v, img_color_frame));
         c.g = std::clamp<unsigned char>(c.g, 0, 255);
 
         for (int i = -1; i <= 2; ++i) {
@@ -142,15 +141,8 @@ public:
                 GetImageColor(img.orgImg, u + i, v + j).b;
           }
         }
-        c.b = std::round(calculate_interpolated_color());
+        c.b = std::round(calculate_interpolated_color(x, y, u, v, img_color_frame));
         c.b = std::clamp<unsigned char>(c.b, 0, 255);
-
-        // const Color c1 = GetImageColor(img.orgImg, u, v);
-        // const Color c2 = GetImageColor(img.orgImg, u + 1, v);
-
-        // c.r = linInt(c1.r, c2.r, x - u);
-        // c.g = linInt(c1.g, c2.g, x - u);
-        // c.b = linInt(c1.b, c2.b, x - u);
 
         zoomed_image_pixels[X + Y * zoom_image_width] = c;
       }
